464243004d1f3807a1a67008f267467765c22557
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-05-19 00:07:27 +0530

    Create Test_2.sv

---
+++ Source/VIP_block_name/sv/tests/Test_2.sv
@@ -0,0 +1,27 @@
+class Test_2 extends TEST;
+
+  transaction trans;
+
+  function new (virtual intf vif);
+    super.new(vif);
+  endfunction
+
+  task run();
+    reset();
+    env.test_run();
+    trans = new();
+    files_pkg :: raise();
+    
+    trans.i_J = 4'd2;
+      trans.i_XTS_START = 1;
+      trans.i_SECTOR = 128'h33333333_33000000_00000000_00000000;
+      trans.i_XTS_PLAINTEXT = 256'h44444444_44444444_44444444_44444444_44444444_44444444_44444444_44444444;
+      trans.i_KEY_1 = 128'h11111111_11111111_11111111_11111111;
+      trans.i_KEY_2 = 128'h22222222_22222222_22222222_22222222;
+
+//       @(negedge i_clk);
+//       i_XTS_START =  0;
+
+      env.gen.send(trans);
+  endtask
+endclass
