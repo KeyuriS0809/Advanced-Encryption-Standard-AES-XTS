2ccd188d98834a9b6b9043e8bcfd9b52a85a9fdf
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-05-26 12:29:30 +0530

    Update All_zero.sv

--- Source/VIP_block_name/sv/tests/All_zero.sv
+++ Source/VIP_block_name/sv/tests/All_zero.sv
@@ -7,22 +7,17 @@ class All_zero extends TEST;
   endfunction
 
   task run();
-    reset();
-    env.test_run();
+
+    env.test_run();      // ENV run call 
     trans = new();
     files_pkg :: raise();
-      trans.i_J = 4'd2;
-      trans.i_XTS_START = 1;
-      trans.i_SECTOR = 128'h00000000_00000000_00000000_00000000;
-      trans.i_XTS_PLAINTEXT = 256'h00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
-      trans.i_KEY_1 = 128'h00000000_00000000_00000000_00000000;
-      trans.i_KEY_2 = 128'h00000000_00000000_00000000_00000000;
+    trans.i_J = 4'd2;
+    trans.i_XTS_START = 1;
+    trans.i_SECTOR = 128'h00000000_00000000_00000000_00000000;
+    trans.i_XTS_PLAINTEXT = 256'h00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
+    trans.i_KEY_1 = 128'h00000000_00000000_00000000_00000000;
+    trans.i_KEY_2 = 128'h00000000_00000000_00000000_00000000;
     env.gen.send(trans);
-    
-//     trans = new();
-//     @(negedge vif.i_clk);
-//     trans.i_XTS_START = 0;
-//     env.gen.send(trans);
 
   endtask
 endclass
