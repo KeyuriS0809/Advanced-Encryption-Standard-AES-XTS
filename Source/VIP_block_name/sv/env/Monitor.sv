c9a5b48d4fd28ed34ae4d77825a2e8c1b8398c44
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-08-16 22:26:29 +0530

    Update Monitor.sv

--- Source/VIP_block_name/sv/env/Monitor.sv
+++ Source/VIP_block_name/sv/env/Monitor.sv
@@ -1,40 +1,44 @@
-class monitor;
+class my_monitor extends uvm_monitor;
+  `uvm_component_utils(my_monitor)
+  
+  my_item m_trans;
+  virtual my_intf vif;
+  uvm_analysis_port #(my_item) m_ap;   
 
-  transaction trans;
-  mailbox #(transaction) mon2score;
-  virtual intf vif;
-
-  function new (mailbox #(transaction) mon2score, virtual intf vif);
-    this.mon2score = mon2score;
-    this.vif = vif;
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
   endfunction
 
-  task main;
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    m_ap = new("m_ap", this);
 
-    forever begin
+    if (!uvm_config_db #(virtual my_intf)::get(this, "", "vif", vif))
+      `uvm_fatal("MON", "No virtual interface in config_db!")
+  endfunction
 
-      if (vif.i_reset) begin
-        $display("----- TIME = %0t Reset Detected in [MONITOR] Reset = %0b  -----",$time, vif.i_reset);
-        wait(!vif.i_reset);
-      end
-      else begin 
-        trans = new();
-        
-        @(vif.o_XTS_DONE);
-        @(posedge vif.i_clk);
-        
-        trans.i_XTS_START      =  vif.i_XTS_START;
-        trans.i_J              =  vif.i_J;
-        trans.i_SECTOR         =  vif.i_SECTOR;
-        trans.i_XTS_PLAINTEXT  =  vif.i_XTS_PLAINTEXT;
-        trans.i_KEY_1          =  vif.i_KEY_1;
-        trans.i_KEY_2          =  vif.i_KEY_2;
-        trans.o_XTS_DONE       =  vif.o_XTS_DONE;
-        trans.o_XTS_CIPHERTEXT =  vif.o_XTS_CIPHERTEXT;
-
-        mon2score.put(trans);      
-        trans.display("MONITOR");
-      end
+  task run_phase(uvm_phase phase);
+    
+  forever begin
+    if (vif.i_reset) begin
+      wait (!vif.i_reset);
     end
-  endtask
+    else begin
+      @(posedge vif.i_clk iff vif.i_XTS_START);   
+      
+      m_trans = my_item::type_id::create("m_trans");
+      m_trans.i_J             = vif.i_J;
+      m_trans.i_SECTOR         = vif.i_SECTOR;
+      m_trans.i_XTS_PLAINTEXT  = vif.i_XTS_PLAINTEXT;
+      m_trans.i_KEY_1          = vif.i_KEY_1;
+      m_trans.i_KEY_2          = vif.i_KEY_2;
+
+      wait(vif.o_XTS_DONE);    // capture output at DONE
+      m_trans.o_XTS_CIPHERTEXT = vif.o_XTS_CIPHERTEXT;
+      m_trans.o_XTS_DONE       = vif.o_XTS_DONE;
+
+      m_ap.write(m_trans);
+     end
+   end
+ endtask
 endclass
