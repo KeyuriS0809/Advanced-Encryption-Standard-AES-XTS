a60c1d927c5b3c685b93fd683c38480ba0c7cd36
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-08-16 22:27:27 +0530

    Update Transaction.sv

--- Source/VIP_block_name/sv/env/Transaction.sv
+++ Source/VIP_block_name/sv/env/Transaction.sv
@@ -1,23 +1,30 @@
-class transaction;
-  bit                         i_clk;
-  bit                         i_reset;
+class my_item extends uvm_sequence_item;
+  `uvm_object_utils(my_item)
+  
   bit                         i_XTS_START;
   bit [`BLOCK_NUMBER-1:0]     i_J;
-  bit [`CIPHERTEXT_WIDTH-1:0] i_SECTOR;
-  bit [`PLAINTEXT_WIDTH-1:0]  i_XTS_PLAINTEXT;
-  bit [`KEY_WIDTH-1:0]        i_KEY_1;
-  bit [`KEY_WIDTH-1:0]        i_KEY_2;
+  
+  rand bit [`CIPHERTEXT_WIDTH-1:0] i_SECTOR;
+  rand bit [`PLAINTEXT_WIDTH-1:0]  i_XTS_PLAINTEXT;
+  rand bit [`KEY_WIDTH-1:0]        i_KEY_1;
+  rand bit [`KEY_WIDTH-1:0]        i_KEY_2;
 
   bit [`PLAINTEXT_WIDTH-1:0] o_XTS_CIPHERTEXT;
-  bit                       o_XTS_DONE;
+  bit                        o_XTS_DONE;
+
+  function void do_print(uvm_printer printer);
+    super.do_print(printer);
+    printer.print_field_int("i_XTS_START", i_XTS_START, $bits(i_XTS_START), UVM_HEX);
+    printer.print_field_int("i_J", i_J, $bits(i_J), UVM_HEX);
+    printer.print_field_int("i_SECTOR", i_SECTOR, $bits(i_SECTOR), UVM_HEX);
+    printer.print_field_int("i_XTS_PLAINTEXT", i_XTS_PLAINTEXT, $bits(i_XTS_PLAINTEXT), UVM_HEX);
+    printer.print_field_int("i_KEY_1", i_KEY_1, $bits(i_KEY_1), UVM_HEX);
+    printer.print_field_int("i_KEY_2", i_KEY_2, $bits(i_KEY_2), UVM_HEX);
+    printer.print_field_int("o_XTS_CIPHERTEXT", o_XTS_CIPHERTEXT, $bits(o_XTS_CIPHERTEXT), UVM_HEX);
+    printer.print_field_int("o_XTS_DONE", o_XTS_DONE, $bits(o_XTS_DONE), UVM_HEX);
+  endfunction
   
-  function void display(string name);
-    $display("\n----------------------------------[ %s ]--------------------------------------",name);
-    $display("TIME = %0t        Total Block  = %d",$time,i_J);
-    $display("TIME = %0t            i_SECTOR = %h",$time,i_SECTOR);
-    $display("TIME = %0t            KEY - 1  = %h",$time,i_KEY_1);
-    $display("TIME = %0t            KEY - 2  = %h",$time,i_KEY_2);
-    $display("TIME = %0t    i_XTS_PLAINTEXT  = %h",$time,i_XTS_PLAINTEXT);
-    $display("TIME = %0t    o_XTS_CIPHERTEXT = %h",$time,o_XTS_CIPHERTEXT);
+  function new(string name = "my_item");
+    super.new(name);
   endfunction
 endclass
