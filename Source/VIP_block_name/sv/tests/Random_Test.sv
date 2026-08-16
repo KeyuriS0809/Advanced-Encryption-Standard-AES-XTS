a851586845bce7dc0599713807eaf34fe8cb9413
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-08-16 22:38:37 +0530

    Update and rename Test.sv to Random_Test.sv

---
+++ Source/VIP_block_name/sv/tests/Random_Test.sv
@@ -0,0 +1,42 @@
+class Random_Test extends uvm_test;
+  `uvm_component_utils(Random_Test)
+
+  Env_config        m_env_cfg;
+  my_env            m_env;
+  virtual my_intf   vif;
+  Random_Test_seq   m_rand_seq;
+  
+  function new (string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    m_env_cfg = Env_config::type_id::create("m_env_cfg");
+    
+    m_env_cfg.scoreboard_enable = 1;
+    m_env_cfg.coverage_enable = 0;
+    m_env_cfg.rand_coverage_enable = 1;
+    
+    uvm_config_db #(Env_config)::set(this, "m_env", "m_env_cfg", m_env_cfg);
+      
+    if (!uvm_config_db #(virtual my_intf)::get(this, "", "vif", vif))
+      `uvm_fatal("TEST", "No virtual interface in config_db!")
+      
+    m_env = my_env::type_id::create("m_env", this);
+  endfunction
+  
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    
+    phase.raise_objection(this);
+    
+    m_rand_seq = Random_Test_seq::type_id::create("m_rand_seq");
+    
+    m_rand_seq.start(m_env.m_agt.m_seqr);
+   
+    phase.drop_objection(this);
+  endtask
+
+endclass
